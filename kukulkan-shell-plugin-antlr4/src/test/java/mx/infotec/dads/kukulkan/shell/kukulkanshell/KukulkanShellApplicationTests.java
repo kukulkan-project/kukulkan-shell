9c5644dc2022a6836058d8a3eba1b2796cf53186
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-01-27 12:00:07 -0600

    KukulkanEngineApp.java
    KukulkanEngineApp.java

--- kukulkan-shell-plugin-antlr4/src/test/java/mx/infotec/dads/kukulkan/shell/kukulkanshell/KukulkanShellApplicationTests.java
+++ kukulkan-shell-plugin-antlr4/src/test/java/mx/infotec/dads/kukulkan/shell/kukulkanshell/KukulkanShellApplicationTests.java
@@ -1,29 +1,35 @@
 package mx.infotec.dads.kukulkan.shell.kukulkanshell;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.ResourceUtils;
+
+import mx.infotec.dads.kukulkan.metamodel.context.GeneratorContext;
+import mx.infotec.dads.kukulkan.shell.commands.antlr4.Antlr4Generator;
+import mx.infotec.dads.kukulkan.shell.commands.antlr4.Antlr4Params;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = KukulkanEngineApp.class)
 public class KukulkanShellApplicationTests {
 
+    @Autowired
+    private Antlr4Generator generator;
+    
     @Test
-    public void readFile() throws IOException {
-        File file = ResourceUtils.getFile("classpath:templates/error.html");
-
-        // File is found
-        System.out.println("File Found : " + file.exists());
-
-        // Read File Content
-        String content = new String(Files.readAllBytes(file.toPath()));
-        System.out.println(content);
+    public void generate() throws IOException {
+        
+        Antlr4Params pConf = new Antlr4Params();
+        pConf.setId("antlr4");
+        pConf.setPackaging("mx.infotec.dads.antlr4");
+        pConf.setOutputDir(Paths.get("/home/daniel/refactoring"));
+        GeneratorContext genCtx = new GeneratorContext();
+        genCtx.put(Antlr4Params.class, pConf);
+        generator.process(genCtx);
     }
     
  
