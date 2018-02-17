e0796755dd16439e38b983f1267acc58bc49846a
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-02-17 00:36:23 -0600

    refactoring templateType
    refactoring templateType

--- kukulkan-shell-plugin-antlr4/src/test/java/mx/infotec/dads/kukulkan/shell/kukulkanshell/KukulkanShellApplicationTests.java
+++ kukulkan-shell-plugin-antlr4/src/test/java/mx/infotec/dads/kukulkan/shell/kukulkanshell/KukulkanShellApplicationTests.java
@@ -1,34 +1,48 @@
 package mx.infotec.dads.kukulkan.shell.kukulkanshell;
 
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import mx.infotec.dads.kukulkan.metamodel.context.GeneratorContext;
 import mx.infotec.dads.kukulkan.shell.commands.Antlr4Args;
 import mx.infotec.dads.kukulkan.shell.generator.Antlr4Context;
 import mx.infotec.dads.kukulkan.shell.generator.Antlr4Generator;
+import mx.infotec.dads.kukulkan.shell.util.Mapper;
+
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = KukulkanEngineApp.class)
+@EnableAutoConfiguration
+@Import(value = { KukulkanEngineApp.class })
+@ComponentScan(basePackages = { "mx.infotec.dads.kukulkan.shell", "mx.infotec.dads.kukulkan.engine",
+        "mx.infotec.dads.kukulkan.metamodel" }, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CommandLineRunner.class))
 public class KukulkanShellApplicationTests {
-
     @Autowired
     private Antlr4Generator generator;
 
     @Test
     public void generate() throws IOException {
-        Antlr4Context pConf = new Antlr4Context();
-        pConf.setId("antlr4");
-        pConf.setPackaging("mx.infotec.dads.antlr4");
-        pConf.setOutputDir(Paths.get("/home/daniel/refactoring"));
+        Antlr4Args params = new Antlr4Args();
+        params.setGrammarExtension("3k");
+        params.setGrammarName("kukulkan");
+        params.setId("kukulkan-grammar-testing");
+        params.setPackaging("mx.dads.infotec");
+        Antlr4Context antlr4Context = Mapper.toContext(params);
+        Path tmpFolder = Files.createTempDirectory("kukulkan-output-test");
+        tmpFolder.toFile().deleteOnExit();
+        antlr4Context.setOutputDir(tmpFolder);
         GeneratorContext genCtx = new GeneratorContext();
-        genCtx.put(Antlr4Args.class, pConf);
+        genCtx.put(Antlr4Context.class, antlr4Context);
         generator.process(genCtx);
     }
 }
